1ad1e86e8730556f24517125bb113e99098e9e3b
Luong Vy <luongtuanvy04@gmail.com>
2025-09-17 13:10:39 +0700

    feat: implement Chat component and integrate with App for message display

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
+import { useState } from "react";
 import styles from "./App.module.css";
+import { Chat } from "../src/components/Chat/Chat.jsx";
 
 function App() {
+  const [messages, setMessages] = useState(MESSAGES);
+
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
         <img className={styles.Logo} src="../public/chat-bot.png" />
         <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
-      <div className={styles.ChatContainer} />
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
     </div>
   )
 }
+
+const MESSAGES = [
+  {
+    role: 'user',
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?"
+  },
+  {
+    role: 'assistant',
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?"
+  },
+  {
+    role: 'user',
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?"
+  },
+  {
+    role: 'assistant',
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?"
+  },
+  {
+    role: 'user',
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?"
+  },
+  {
+    role: 'assistant',
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus voluptatibus ipsam illo eligendi obcaecati alias magni voluptate, corrupti adipisci quae esse fugiat nesciunt suscipit sunt laboriosam fuga aperiam sed. Sint?"
+  }
+]
 
 export default App
