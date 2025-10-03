394f6576ab96ff214068808a79125e72dee64980
Luong Vy <luongtuanvy04@gmail.com>
2025-10-03 14:57:43 +0700

    feat: implement loading indicator and update assistant integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Assistant } from "./assistants/openai.js";
+import { Assistant } from "./assistants/googleai.js";
+import { Loader } from "./components/Loader/Loader.jsx";
 import styles from "./App.module.css";
 import { Chat } from "../src/components/Chat/Chat.jsx";
 import { Controls } from "./components/Controls/Controls.jsx";
@@ -7,6 +8,7 @@ import { Controls } from "./components/Controls/Controls.jsx";
 function App() {
   const assistant = new Assistant();
   const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function addMessage(message) {
     setMessages((prevMessages) => [...prevMessages, message])
@@ -14,19 +16,23 @@ function App() {
 
   async function handleContentSend(content) {
     addMessage({ content, role: "user" });
+    setIsLoading(true);
     try {
-      const result = await assistant.chat(content, messages)
+      const result = await assistant.chat(content)
       addMessage({ content: result, role: "assistant" })
     } catch (error) {
       addMessage({
         content: "Sorry, I couldn't process your request. Please try again.",
         role: "system"
       })
+    } finally {
+      setIsLoading(false);
     }
   }
 
   return (
     <div className={styles.App}>
+      {isLoading && <Loader />}
       <header className={styles.Header}>
         <img className={styles.Logo} src="../public/chat-bot.png" />
         <h2 className={styles.Title}>AI Chatbot</h2>
